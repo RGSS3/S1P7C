114b2867d2e3330697f9c5f7048b44c98a32c97f
you <you>
2020-08-28 02:49:38 +0800

    fixed example

--- tests/2.c
+++ tests/2.c
@@ -23,28 +23,33 @@ int main() {
         w->lpWindowName = "Hello OpenGL";
     }
     
+    glViewport(0, 0, 640, 480);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0, 0, 10, 10);
+    gluOrtho2D(0, 640, 0, 480);
+    glMatrixMode(GL_MODELVIEW);  
+    glLoadIdentity(); 
+    
     
     float a = 0.1f;
     const float fps = 60.0f;
+    const float velocity = 20.0f;
     spcgl_loop_dt(dt) {
     	if (!spc_timespan_mark(&dt, 1.0f/fps)) {
     		continue;	
     	}
-    	a += 0.1f*dt.span;
+    	a += velocity * dt.span;
     	glClearColor(0.4f, 0.8f, 1.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_TRIANGLES);
 		glColor3f(1.0f, 0.0f, 0.0f);
 		glVertex2f(a, 0);
 		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(a + 1, 0);
+		glVertex2f(a + 100, 0);
 		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(a + 1, 1);
+		glVertex2f(a + 100, 100);
 		glEnd();
-		if (a > 1) a = -1;
+		if (a > 640) a = 0;
     }
     return 0;
 }
